c50cf4446bdc7f02bd5b7721073941925df05c65
Helen Han <helen-hjy@outlook.com>
2021-04-10 20:03:55 +1000

    upload project files

---
+++ 90015-Ass1/src/client/DictionaryClient.java
@@ -0,0 +1,223 @@
+package client;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.awt.Font;
+import javax.swing.JTextArea;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+@SuppressWarnings("serial")
+public class DictionaryClient extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField TextField;
+	private static JTextArea TextArea;
+	private static String inputtext; //input text in textfield
+	private static String operation; //operation command pass to server
+	private static String result; 	 //result string shown in TextArea
+	private static String ip;
+	private static int port;
+	private static Socket socket;
+	
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		//get arguments from cmd
+		ip = args[0]; //server ip address
+		port = Integer.parseInt(args[1]); //port number
+	
+		//invoke the frame
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					DictionaryClient frame = new DictionaryClient();
+					frame.setTitle("MyDict by Jiayu Han");
+					frame.setVisible(true);
+				} catch (Exception e) {
+					System.out.println("Cannot invoke Jframe for client GUI!");
+				}
+			}
+		});
+		
+	}
+	//create thread per request
+	public static void createThread() {
+		//run socket
+		try(Socket socket = new Socket(ip, port);)
+		{
+			//create Output and Input Stream
+			DataInputStream input = new DataInputStream(socket.getInputStream());
+			DataOutputStream output = new DataOutputStream(socket.getOutputStream());
+			
+			//retrieve operation and input text in textField
+			String Data = operation + ":" + inputtext;	
+			
+			//write data into output stream and pass to server
+			output.writeUTF(Data);
+		 	output.flush();
+	    	
+	    	result=input.readUTF();
+		} 
+		catch (UnknownHostException e)
+		{
+			System.out.println("IP address of a host could not be determined.");
+		}
+		catch (IOException e) 
+		{
+			System.out.println("Cannot run socket.");
+		}
+	}
+	/**
+	 * Create the frame.
+	 */
+	
+	public DictionaryClient() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		GridBagLayout gbl_contentPane = new GridBagLayout();
+		gbl_contentPane.columnWeights = new double[]{0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
+		gbl_contentPane.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 1.0};
+		contentPane.setLayout(gbl_contentPane);
+		
+		JLabel lblNewLabel = new JLabel("Welcome to MyDict!");
+		lblNewLabel.setFont(new Font("Book Antiqua", Font.ITALIC, 20));
+		GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
+		gbc_lblNewLabel.gridwidth = 11;
+		gbc_lblNewLabel.insets = new Insets(0, 0, 5, 0);
+		gbc_lblNewLabel.gridx = 0;
+		gbc_lblNewLabel.gridy = 0;
+		contentPane.add(lblNewLabel, gbc_lblNewLabel);
+		
+		TextArea = new JTextArea();
+		TextArea.setToolTipText("Output display");
+		TextArea.setFont(new Font("Book Antiqua", Font.PLAIN, 13));
+		GridBagConstraints gbc_TextArea = new GridBagConstraints();
+		gbc_TextArea.insets = new Insets(0, 0, 0, 5);
+		gbc_TextArea.gridwidth = 9;
+		gbc_TextArea.fill = GridBagConstraints.BOTH;
+		gbc_TextArea.gridx = 1;
+		gbc_TextArea.gridy = 4;
+		contentPane.add(TextArea, gbc_TextArea);
+		
+		TextField = new JTextField();
+		TextField.setToolTipText("Input a word or a word with its definition separated by comma");
+		TextField.setFont(new Font("Book Antiqua", Font.PLAIN, 13));
+		GridBagConstraints gbc_TextField = new GridBagConstraints();
+		gbc_TextField.gridwidth = 9;
+		gbc_TextField.insets = new Insets(0, 0, 5, 5);
+		gbc_TextField.fill = GridBagConstraints.HORIZONTAL;
+		gbc_TextField.gridx = 1;
+		gbc_TextField.gridy = 1;
+		contentPane.add(TextField, gbc_TextField);
+		TextField.setColumns(10);
+		//query button and event handler
+		JButton QueryBtn = new JButton("Query");
+		QueryBtn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				//get text in textField and pass it to input stream
+				inputtext = TextField.getText();
+				//define operation string
+				operation = "query";
+				//run socket
+				createThread();
+				//show result
+				TextArea.setText(result);
+			}
+				
+		});
+		
+		QueryBtn.setFont(new Font("Book Antiqua", Font.PLAIN, 13));
+		GridBagConstraints gbc_QueryBtn = new GridBagConstraints();
+		gbc_QueryBtn.insets = new Insets(0, 0, 5, 5);
+		gbc_QueryBtn.gridx = 1;
+		gbc_QueryBtn.gridy = 2;
+		contentPane.add(QueryBtn, gbc_QueryBtn);
+		//add button
+		JButton AddBtn = new JButton("Add");
+		AddBtn.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				//get text in textField and pass it to input stream
+				inputtext = TextField.getText();
+				//define operation string
+				operation = "add";
+				//run socket
+				createThread();
+				//show result
+				TextArea.setText(result);
+			}
+			
+		});
+		AddBtn.setFont(new Font("Book Antiqua", Font.PLAIN, 13));
+		GridBagConstraints gbc_AddBtn = new GridBagConstraints();
+		gbc_AddBtn.anchor = GridBagConstraints.WEST;
+		gbc_AddBtn.insets = new Insets(0, 0, 5, 5);
+		gbc_AddBtn.gridx = 2;
+		gbc_AddBtn.gridy = 2;
+		contentPane.add(AddBtn, gbc_AddBtn);
+		//remove button
+		JButton RemoveBtn = new JButton("Remove");
+		RemoveBtn.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				//get text in textField and pass it to input stream
+				inputtext = TextField.getText();
+				//define operation string
+				operation = "remove";
+				//run socket
+				createThread();
+				//show result in textarea
+				TextArea.setText(result);
+			}
+		});
+		RemoveBtn.setFont(new Font("Book Antiqua", Font.PLAIN, 13));
+		GridBagConstraints gbc_RemoveBtn = new GridBagConstraints();
+		gbc_RemoveBtn.insets = new Insets(0, 0, 5, 5);
+		gbc_RemoveBtn.gridx = 8;
+		gbc_RemoveBtn.gridy = 2;
+		contentPane.add(RemoveBtn, gbc_RemoveBtn);
+		//update event handler
+		JButton UpdateBtn = new JButton("Update");
+		UpdateBtn.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				//get text in textField and pass it to input stream
+				inputtext = TextField.getText();
+				//define operation string
+				operation = "update";
+				//run socket
+				createThread();
+				//show result in textarea
+				TextArea.setText(result);
+			}
+		});
+		UpdateBtn.setFont(new Font("Book Antiqua", Font.PLAIN, 13));
+		GridBagConstraints gbc_UpdateBtn = new GridBagConstraints();
+		gbc_UpdateBtn.insets = new Insets(0, 0, 5, 5);
+		gbc_UpdateBtn.gridx = 9;
+		gbc_UpdateBtn.gridy = 2;
+		contentPane.add(UpdateBtn, gbc_UpdateBtn);
+		
+	}
+
+}
---
+++ 90015-Ass1/src/server/DictionaryServer.java
@@ -0,0 +1,300 @@
+package server;
+
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import javax.net.ServerSocketFactory;
+
+public class DictionaryServer {
+	
+	private static int counter = 0; //count the client
+	private static String filepath; //get filepath
+	private static int port; //get port number
+
+ 	public static void main(String[] args)
+	{	
+		ServerSocketFactory factory = ServerSocketFactory.getDefault();
+		// get arguments
+		port = Integer.parseInt(args[0]); //port number
+		filepath = args[1]; //dictionary path
+		
+		try(ServerSocket server = factory.createServerSocket(port))
+		{
+			// Wait for connections for each client
+			while(true)
+			{
+				Socket client = server.accept();//accept the client
+				counter++;			 			// get a new client
+				System.out.println("Thread "+counter+": connection successful!");
+				
+				// Start a new thread for each request
+				Thread t = new Thread(() -> serveClient(client));
+				t.start();
+			}
+			
+		} 
+		catch (IOException e)
+		{
+			System.out.println("Socket failed to create! (line 27)");
+		}
+		
+	}
+	//run the client
+	private static void serveClient(Socket client)
+	{
+		try(Socket clientSocket = client)
+		{
+			// Input stream
+			DataInputStream input = new DataInputStream(clientSocket.getInputStream());
+			// Output Stream
+		    DataOutputStream output = new DataOutputStream(clientSocket.getOutputStream());
+		    
+		    //receive command from the server
+		    String command = input.readUTF(); //get text from input stream
+		    if(command.contains(":")) {
+		    	String receiver[] = command.split(":"); //split operation and text
+			   
+			    //process four operations and write into output stream
+			    if (receiver[0].equals("query")){   
+			    	//output in terminal
+					System.out.println(" Thread "+counter+": Process query...");
+			    	output.writeUTF(query(receiver[1]));
+			    	
+			    }
+			    if(receiver[0].equals("add")) {
+					//output in terminal
+					System.out.println(" Thread "+counter+": Process adding... ");
+			    	output.writeUTF(add(receiver[1]));
+			    	
+			    }
+			    if(receiver[0] .equals( "remove")) {
+			    	//output in terminal
+					System.out.println(" Thread "+counter+": Process removal...");
+			    	output.writeUTF(remove(receiver[1]));
+			    	
+			    }
+			    if(receiver[0] .equals( "update"))
+			    {
+			    	//output in terminal
+					System.out.println(" Thread "+counter+": Process update...");
+			    	output.writeUTF(update(receiver[1]));
+			    }
+		    }
+		    else {
+		    	System.out.println("Error: not valid command - no :");
+		    	
+		    }
+		    
+		    
+		} 
+		catch (IOException e) 
+		{
+			System.out.println("Socket failed to run! (line 51)");
+		}
+	}
+	
+	//process query 
+	public static String query(String input) {
+		input = input.trim();//remove all space
+		
+		String message = "";
+		//default message
+		message = "Error: No such word: \"" + input + "\" in dictionary file.";
+		
+		//error: not valid input
+		if(input.contains(",")) {
+			message = "Error: Not valid input - should not include comma for query.";
+		}
+		else {
+			try {
+				String line ="";	
+				BufferedReader reader = new BufferedReader(new FileReader(filepath));		
+				while((line = reader.readLine()) != null)
+				{
+					  String[] fields = line.split(",");
+					  //find the query word
+					  if(fields[0].trim().equals(input)) 
+					  {
+						  //return the explanation
+						  message = fields[1].trim();					  
+						  message = "Explanation found: " + message;
+					  }
+				}
+			} catch (FileNotFoundException e) {
+				System.out.println("Have not found file in: "+filepath);		
+			} catch (IOException e) {
+				System.out.println("Cannot read the file "+filepath+" from Bufferreader (line 117)");
+			}
+		}
+		
+		System.out.println(" Operation result: "+message);
+		return message;	
+		
+	}
+	
+	//add a new word
+	public static String add(String input) {
+		String message = "";
+		input = input.trim();		// remove all space
+			
+		try {
+			FileWriter writer = new FileWriter(filepath, true); //append file by filewriter
+			
+			//split the word
+			if(input.contains(","))
+			{	
+				String text[] = input.split(",");			
+				//error: no explanation
+				if(text.length==1) {
+					message = "Error: empty explanation.";
+				}
+				//while valid input
+				else {				
+					writer.append(text[0].trim()+","+text[1].trim()+"\n");				
+					writer.close();
+					//update message
+					message = "New word: " + text[0] + " with its explanation is added!";
+				}
+				
+			}
+			//if word has no comma
+			else {
+				
+				message = "Error: Not valid input - no comma separating word and explanation.";
+			}
+			
+		} catch (IOException e) {
+			System.out.println("Cannot write file in: "+filepath);
+		}
+		
+		System.out.println(" Operation result: "+message);
+		return message; 
+	}
+	
+	//remove a word
+	public static String remove(String input) {
+		input = input.trim();
+		
+		String message = "";	
+		//default message
+		message = "Error: No such word: \"" + input + "\" in dictionary file.";
+		
+		//error: not valid input
+		if(input.contains(",")) {
+			message = "Error: Not valid input - should not include comma for removal";
+		}
+		//with valid input
+		else {
+			try {
+				String line ="";
+				String file = "";
+				@SuppressWarnings("resource")
+				BufferedReader reader = new BufferedReader(new FileReader(filepath));
+							
+				while((line = reader.readLine()) != null)
+				{			
+					  String[] fields = line.split(",");
+					  fields[0] = fields[0].trim();
+					  fields[1] = fields[1].trim();					  
+					  //search the word
+					  if(fields[0].trim().equals(input)) {						  
+						  file += "";		//replace word with null		  
+						  message = "Word: "+input+" removed successfully!";		  		  
+					  }
+					  else {
+						  //if not the word to remove, write into file
+						  file = file + line + "\n";
+					  }					  
+				}
+				
+				//overwrites the file
+				FileWriter fw = new FileWriter(filepath,false); 
+				fw.write(file);		
+				fw.close();
+				
+			} catch (FileNotFoundException e) {
+				System.out.println("Have not found file in: "+filepath);		
+			} catch (IOException e) {
+				System.out.println("Cannot read the file "+filepath+" from Bufferreader (line 117)");
+			}
+		}
+		
+		System.out.println(" Operation result: "+message);
+		return message;	
+	}
+	
+	//update explanation of a word
+	public static String update(String input) {
+		input = input.trim(); //remove space
+	
+		String message = "";	
+
+		//input contains comma
+		if(input.contains(",")) {
+			String[] text = input.split(","); //split input into text[]
+			
+			//default message
+			message = "Error: No such word: \"" + text[0] + "\" in dictionary file.";		
+			
+			//error: no explanation
+			if(text.length==1) {
+				message = "Error: empty explanation.";
+			}
+			
+			//remove spaces
+			text[0] = text[0].trim();
+			text[1] = text[1].trim();
+					
+			//when valid input
+			try {
+				String line ="";
+				String file = "";
+				BufferedReader reader = new BufferedReader(new FileReader(filepath));
+				//read the file line by line
+				while((line = reader.readLine()) != null)
+				{									
+					String[] fields = line.split(","); //split the word and explanation
+					//remove all space
+					fields[0] = fields[0].trim();
+					fields[1] = fields[1].trim();
+				    //compare the word with input
+				    if(fields[0].equals(text[0])) 
+				    {						  
+					    file = file + fields[0] + "," + text[1] + "\n";	 //update explanation	
+					    //update message
+					    message = "Update \"" + fields[0] + "\" explanation successfully!";
+				    }
+				    //if the line is not the word, append into string-"file"
+				    else {			   
+					    file = file + line + "\n";
+				    }					  
+				}
+				
+				//overwrites the file
+				FileWriter fw = new FileWriter(filepath,false); 
+				fw.write(file);		
+				fw.close();
+				
+			} catch (FileNotFoundException e) {
+				System.out.println("Have not found file in: "+filepath);		
+			} catch (IOException e) {
+				System.out.println("Cannot read the file "+filepath+" from Bufferreader (line 117)");
+			}
+		}
+		//when no comma
+		else {
+			message = "Error: Not valid input - should use comma to separate word and explanation.";
+		}
+		
+		System.out.println(" Operation result: "+message);
+		return message;	
+	}
+
+}
+
